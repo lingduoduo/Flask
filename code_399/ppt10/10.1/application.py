4b01aa84be4430781bf9b16a75fc5597cc5e2354
Ling Huang <linghypshen@gmail.com>
2022-09-09 00:04:02 -0400

    add code

---
+++ code_399/ppt10/10.1/application.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_apscheduler import APScheduler
+import os
+app = Flask( __name__ )
+
+manager = Manager( app )
+
+app.config.from_pyfile( "config/base_setting.py" )
+#ops_config=local|production
+#linux export ops_config=local|production
+#windows set ops_config=local|production
+if "ops_config" in os.environ:
+    app.config.from_pyfile( "config/%s_setting.py"%( os.environ['ops_config'] ) )
+
+db = SQLAlchemy( app )
+
+scheduler = APScheduler()
+scheduler.init_app( app )
+
+
+
---
+++ code_399/ppt10/10.1/manager.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from application import app,manager,scheduler
+from flask_script import Server,Command
+from www import *
+
+##web server
+manager.add_command( "runserver",Server( host = "0.0.0.0",use_debugger=True,use_reloader= True ) )
+
+##create_table
+@Command
+def create_all():
+    from application import db
+    from common.models.user import User
+    db.create_all()
+
+manager.add_command( "create_all",create_all )
+
+def aps_test():
+    import datetime
+    print( datetime.datetime.now().strftime( "%Y-%m-%d %H:%M:%S" ) )
+
+
+def main():
+    app.apscheduler.add_job( func = aps_test,trigger="cron",second = "*/5",id = "aps_test" )
+    scheduler.start()
+    manager.run()
+
+if __name__ == "__main__":
+    #app.run( host = "0.0.0.0",debug=True )
+    try:
+        import sys
+        sys.exit( main() )
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
