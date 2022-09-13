637000cbde8cc9e7dc6fc5b47df4a0c756a19ddf
Ling Huang <linghypshen@gmail.com>
2022-09-12 23:21:31 -0400

    add code

--- code_399/ppt4/4.3/index_2.py
+++ code_399/ppt4/4.3/index_2.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 
-app = Flask( __name__ )
+app = Flask(__name__)
 
-@app.route( "/" )
+
+@app.route("/")
 def hello():
     return "Hello, I Love Imooc"
 
+
 if __name__ == "__main__":
-    app.run( host = "0.0.0.0",debug = True )
+    app.run(host="0.0.0.0", debug=True)
