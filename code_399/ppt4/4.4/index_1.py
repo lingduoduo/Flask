a00dd060e767549111878a3a935d15ba285dba38
Ling Huang <linghypshen@gmail.com>
2023-02-07 16:13:33 -0500

    add code

--- code_399/ppt4/4.4/index_1.py
+++ code_399/ppt4/4.4/index_1.py
@@ -14,4 +14,4 @@ class Shortly(object):
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
     app = Shortly()
-    run_simple( '0.0.0.0',5000, app)
+    run_simple('0.0.0.0', 5000, app)
