e7c6cf2d1f6a9c37c5fdc3fd4ee2844926cc5c7c
Ling Huang <linghypshen@gmail.com>
2023-02-07 17:30:32 -0500

    add code

--- code_399/ppt4/4.4/index_1.py
+++ code_399/ppt4/4.4/index_1.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 from werkzeug.wrappers import Request,Response
 
-class Shortly(object):
+class example(object):
     def __call__(self, environ,start_response):
-        #start_response( '200 OK',[ ( 'Content-Type','text/plain' ) ] )
-        #return [ b'hello world~~' ]
-        request = Request( environ )
-        text = "hello world, %s"%( request.args.get( 'a','i love Python' ) )
-        response = Response(text,mimetype="text/plain"  )
-        return response( environ,start_response )
+        start_response( '200 OK',[ ( 'Content-Type','text/plain' ) ] )
+        return [ b'hello world~~' ]
+
+        # request = Request( environ )
+        # text = "hello world, %s"%( request.args.get( 'a','i love Python' ) )
+        # response = Response(text,mimetype="text/plain"  )
+        # return response( environ,start_response )
 
 
 if __name__ == "__main__":
     from werkzeug.serving import run_simple
-    app = Shortly()
+    app = example()
     run_simple('0.0.0.0', 5000, app)
