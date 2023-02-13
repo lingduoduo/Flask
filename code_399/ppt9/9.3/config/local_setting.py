0648a8f6c10ff3fb6b74bf39b74167ca5bbe777e
Ling Huang <linghypshen@gmail.com>
2023-02-13 15:16:15 -0500

    add code

--- code_399/ppt9/9.3/config/local_setting.py
+++ code_399/ppt9/9.3/config/local_setting.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 #本地开发环境配置文件
 from config.base_setting import *
-#SQLALCHEMY_ECHO = True
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = "mysql://root:123456@127.0.0.1/movie_cat"
-
-SECRET_KEY = "imooc123456"
-
-
+SECRET_KEY = "python123456"
 DOMAIN = {
-    "www":"http://192.168.0.124:5000"
+    "www":"http://192.168.0.124"
 }
 
 
