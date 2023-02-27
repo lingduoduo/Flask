37e83a71cdece1d3a152b9a5f6bddd021cbe4415
Ling Huang <linghypshen@gmail.com>
2023-02-27 12:53:13 -0500

    add code

--- code_399/ppt9/9.4/controllers/member.py
+++ code_399/ppt9/9.4/controllers/member.py
@@ -21,17 +21,17 @@ def reg():
     login_pwd2 = req['login_pwd2'] if "login_pwd2" in req else ""
 
     if login_name is None or len( login_name ) < 1:
-        return ops_renderErrJSON( msg = "请输入正确的登录用户名~~" )
+        return ops_renderErrJSON( msg="Please input authorized user name~~" )
 
     if login_pwd is None or len( login_pwd ) < 6:
-        return ops_renderErrJSON( msg ="请输入正确的登录密码，并且不能小于6个字符~~")
+        return ops_renderErrJSON( msg="Please input password which is not less than 6 characters~")
 
     if login_pwd != login_pwd2:
-        return ops_renderErrJSON(msg="请输入正确的确认登录密码~~")
+        return ops_renderErrJSON(msg="Please confirmed your password~")
 
     user_info = User.query.filter_by( login_name = login_name ).first()
     if user_info:
-        return ops_renderErrJSON( msg ="登录用户名已被注册，请换一个~~")
+        return ops_renderErrJSON( msg ="Use name has been registered, pls use another new user name ~~")
 
     model_user = User()
     model_user.login_name = login_name
@@ -41,7 +41,7 @@ def reg():
     model_user.created_time = model_user.updated_time = getCurrentTime()
     db.session.add( model_user )
     db.session.commit()
-    return ops_renderJSON( msg = "注册成功~~" )
+    return ops_renderJSON( msg = "Registration Succeed~~" )
 
 
 @member_page.route("/login",methods = [ "GET","POST" ])
@@ -53,22 +53,22 @@ def login():
     login_name = req['login_name'] if 'login_name' in req else ''
     login_pwd = req['login_pwd'] if 'login_pwd' in req else ''
     if login_name is None or len( login_name ) < 1:
-        return ops_renderErrJSON(  "请输入正确的登录用户名~~" )
+        return ops_renderErrJSON(  "Please input authorized user name~~"  )
 
     if login_pwd is None or len( login_pwd ) < 6:
-        return ops_renderErrJSON("请输入正确的登录密码~~")
+        return ops_renderErrJSON("Please input password which is not less than 6 characters~")
     user_info = User.query.filter_by( login_name = login_name ).first()
     if not user_info:
-        return ops_renderErrJSON("请输入正确的登录用户名和密码 -1~~")
+        return ops_renderErrJSON("Please input valid username and password -1~~")
 
     if user_info.login_pwd != UserService.genePwd( login_pwd,user_info.login_salt ):
-        return ops_renderErrJSON("请输入正确的登录用户名和密码 -2 ~~")
+        return ops_renderErrJSON("Please input valid username and password -2 ~~")
 
     if user_info.status != 1:
-        return ops_renderErrJSON( "账号被禁用，请联系管理员处理~~" )
+        return ops_renderErrJSON( "No user info, contact admin ~~" )
 
     #session['uid'] = user_info.id
-    response = make_response( ops_renderJSON( msg="登录成功~~" ) )
+    response = make_response( ops_renderJSON( msg="Login succeeded~~" ) )
     response.set_cookie(app.config['AUTH_COOKIE_NAME'],
                         "%s#%s"%( UserService.geneAuthCode( user_info ),user_info.id ),60 * 60 *24 *120 )
     return response
@@ -79,5 +79,3 @@ def logOut():
     response = make_response( redirect( UrlManager.buildUrl("/") ) )
     response.delete_cookie(  app.config['AUTH_COOKIE_NAME'] )
     return response
-
-
